0357d64ae659b25af905cbb3e2c4248e3ec88647
Hendrik Sauer <hendrik-sauer@mailbox.org>
2026-04-15 15:27:27 +0200

    feat: add arg max projection

--- src/sesn/__init__.py
+++ src/sesn/__init__.py
@@ -1,1 +1,1 @@
-from .models.impl.ses_conv import SESConv_H_H, SESConv_Z2_H, SESConv_H_H_1x1, SESMaxProjection
+from .models.impl.ses_conv import SESConv_H_H, SESConv_Z2_H, SESConv_H_H_1x1, SESMaxProjection, SESArgMaxProjection
